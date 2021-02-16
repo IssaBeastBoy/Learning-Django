8798bb0705ec9aeed29042e911691d1613dff47c
IssaBeastBoy <tshabalalaboyt@gmail.com>
2021-02-16 12:43:35 +0200

    profiles attributes

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -5,7 +5,6 @@ class Profile(models.Model):
     Name = models.CharField(max_length=20, blank=False)
     Surname = models.CharField(max_length=20, blank=False)
     Field = models.CharField(max_length=20, blank=False)
-    
     Institution = models.CharField(max_length=20, blank=False)
     Interest =models.TextField()
     
