c8c729dbf44e241459c3f80e121576fb9113b9b1
IssaBeastBoy <tshabalalaboyt@gmail.com>
2021-02-16 12:34:59 +0200

    source files

---
+++ src/profiles/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Profile(models.Model):
+    Name = models.CharField(max_length=20, blank=False)
+    Surname = models.CharField(max_length=20, blank=False)
+    Field = models.CharField(max_length=20, blank=False)
+    
+    Institution = models.CharField(max_length=20, blank=False)
+    Interest =models.TextField()
+    
