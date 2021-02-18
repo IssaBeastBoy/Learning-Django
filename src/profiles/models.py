560da6c21aa0bf533c655e48e146111843929e20
IssaBeastBoy <tshabalalaboyt@gmail.com>
2021-02-18 16:32:18 +0200

    login set

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -4,8 +4,11 @@ from django.db import models
 class Profile(models.Model):
     Name = models.CharField(max_length=20, blank=False)
     Surname = models.CharField(max_length=20, blank=False)
+    Email = models.EmailField(blank=False)
     Field = models.CharField(max_length=20, blank=False)
     Institution = models.CharField(max_length=20, blank=False)
     Interest =models.TextField()
     Applications = models.TextField()
+    Password = models.TextField(blank=False)
+    
     
--- src/profiles/views.py
+++ src/profiles/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .form import LoginForm
+from .models import Profile
+
+def User_login(request):
+    form = LoginForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    context = {
+        "form" : form
+    }
+    return render(request, "login.html", context)
