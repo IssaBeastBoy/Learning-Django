65d6b5743d2584de2a73a47ace637034c2a44133
IssaBeastBoy <tshabalalaboyt@gmail.com>
2021-02-26 16:39:21 +0200

    login validation

--- src/pages/views.py
+++ src/pages/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
+from profiles.models import Profile
 
 # Create your views here.
 def homepage(request):
-    return render(request, "home.html", {})
+    stop = 1
+    Passwords = []
+    Emails = []
+    user_Name = ""
+    while(stop <= Profile.objects.count()):
+        users = Profile.objects.get(id=stop)
+        Passwords.append(users.Password)
+        Emails.append(users.Email)
+        stop = stop +1
+    context={
+        "Emails": Emails,
+        "Passwords": Passwords,
+    }
+    return render(request, "home.html", context)
 
 def login(request):
     return render(request, "login.html", {})
--- src/profiles/views.py
+++ src/profiles/views.py
@@ -15,34 +15,11 @@ def User_login(request):
         Passwords.append(users.Password)
         Emails.append(users.Email)
         stop = stop +1
-    user_Password = request.POST.get("Password")
-    user_Email = request.POST.get("Email")
-    if user_Email is not None:
-        bool_countEmail = 0
-        bool_countPassword = 0
-        count = 0        
-        while count < len(Emails):
-            if user_Email == Emails[count]:
-                bool_countEmail = bool_countEmail + 1
-                users = Profile.objects.get(id=count+1)
-                user_Name = str(users.Name) + " " + str(users.Surname)
-            if user_Password == Passwords[count]:
-                bool_countPassword = bool_countPassword + 1
-            count = count + 1
-            if count == len(Emails):
-                if bool_countEmail != 1 and bool_countPassword != 1:
-                    return render(request, "login wrgBoth.html", {})
-                elif bool_countEmail == 1 and bool_countPassword != 1:
-                    return render(request, "login wrgPass.html", {})
-                elif bool_countEmail != 1 and bool_countPassword == 1:
-                    return render(request, "login wrgEmail.html", {})
-                else:
-                    context ={
-                        "UserName": user_Name
-                    }                    
-                    return render(request, "welcome.html",context)
-           
-    return render(request, "login.html", {})
+    context={
+        "Emails": Emails,
+        "Passwords": Passwords,
+    }
+    return render(request, "home.html", context)
 
 
 def User_create(request):
